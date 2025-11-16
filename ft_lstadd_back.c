db1cb6fdede433bfca7bef04dc3e980f0036859f
Ionut Tanvuia <itanvuia@c1r7s1.42london.com>
2025-11-16 12:46:11 +0000

    input check

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: itanvuia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 13:50:09 by itanvuia          #+#    #+#             */
-/*   Updated: 2025/10/25 13:50:09 by itanvuia         ###   ########.fr       */
+/*   Updated: 2025/11/16 12:45:32 by itanvuia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*tmp;
 
+	if (!lst || !new)
+		return ;
 	if (!*lst)
 	{
 		*lst = new;
